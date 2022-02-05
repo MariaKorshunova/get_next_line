74fbdb497355703ff8f51d139a60af4eafe92d71
Jone Mabel <jmabel@oa-j5.msk.21-school.ru>
2022-02-05 15:26:52 +0300

    include libft

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: jmabel <jmabel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 19:41:30 by jmabel            #+#    #+#             */
-/*   Updated: 2021/11/20 16:51:43 by jmabel           ###   ########.fr       */
+/*   Updated: 2022/02/05 15:22:21 by jmabel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,8 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <limits.h>
-
-size_t	ft_strlen(const char *str);
+# include "../libft/libft.h"
 
 char	*get_next_line(int fd);
-char	*ft_current_str(int fd, char *current_str);
-char	*ft_new_current_str(char *current_str);
-char	*ft_next_line(char *current_str);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin_gnl(char *str, char *buf);
 
 #endif
